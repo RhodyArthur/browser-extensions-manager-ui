0bb82b5eff464a00e093196efcd8d5bb28265afb
RhodyArthur <rhodaarthur48@gmail.com>
2025-05-30 10:57:56 +0000

    feat: display content dynamically

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const contentEl = document.getElementById('content');
 
-// let render =
-//  `
-
-// `
 
 async function getData() {
     const response = await fetch('assets/data.json');
     const data = await response.json();
     return data;
 }
+
+(async () => {
+    const extensions = await getData();
+
+    extensions.map(extension => {
+        contentEl.innerHTML +=
+        `
+            <div class="card">
+                    <div class="col">
+                        <div class="icon">
+                            <img src="${extension.logo}" alt="${extension.name}">
+                        </div>
+
+                        <div class="details">
+                            <h3 class="name">${extension.name}</h5>
+                            <p class="description">${extension.description}</p>
+                        </div>
+                    </div>
+
+                    <div class="actions">
+                        <button class="btn">Remove</button>
+
+                        <div class="toggle-container">
+                            <input type="checkbox" id="toggle" class="toggle-checkbox">
+                            <label for="toggle" class="toggle-label"></label>
+                        </div>
+                    </div>
+
+                </div>
+        `
+    })
+})()
